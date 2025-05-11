50e367d114f979b03546e8acb76e141097b703cb
Onur Şavkı <42342278+onursavki@users.noreply.github.com>
2025-05-11 16:10:21 +0300

    blockquote.js

--- blockquote.js
+++ blockquote.js
@@ -1,38 +1,37 @@
 document.addEventListener('DOMContentLoaded', () => {
     // .application-form sınıfı içeren elementler üzerinde işlem yap
-    for (let i = 1; i <= 40; i++) { // 1'den 40'a kadar
+    for (let i = 1; i <= 40; i++) {
         const downloadLink = document.getElementById(`downloadLink${i}`);
-        const applicationStatus = document.getElementById('application-status'); // Blockquote alanı
+        const applicationStatus = document.getElementById('application-status');
 
         if (downloadLink) {
-            const button = downloadLink.querySelector('.application-form'); // .application-form sınıfını kontrol et
+            const button = downloadLink.querySelector('.application-form');
 
-            // Eğer href yoksa, butonu devre dışı bırak ve mesajı güncelle
             if (button && !downloadLink.getAttribute('href')) {
                 button.classList.add('disabled');
-                downloadLink.classList.add('cursor-default'); // cursor-default sınıfını ekle
+                downloadLink.classList.add('cursor-default');
 
                 if (applicationStatus) {
                     applicationStatus.innerHTML = `
-                        <h4>🔴 EKİP ALIMLARI KAPALI</h4>
+                        <h4><i class="bi bi-envelope-x" style="color: red;"></i> EKİP ALIMLARI KAPALI</h4>
                         <p>Üzgünüz, şu an Ekip alımlarımız kapalı; sizi aramızda görmek için sabırsızlanıyoruz, bizi takipte kalın!</p>
                         <footer>Ekip Games™</footer>
                     `;
                 }
             } else if (button) {
                 button.classList.remove('disabled');
-                downloadLink.classList.remove('cursor-default'); // cursor-default sınıfını kaldır
+                downloadLink.classList.remove('cursor-default');
 
                 if (applicationStatus) {
                     applicationStatus.innerHTML = `
-                        <h4>🟢 EKİP ALIMLARI AÇIK</h4>
+                        <h4><i class="bi bi-envelope-paper" style="color: green;"></i> EKİP ALIMLARI AÇIK</h4>
                         <p>Başvurunuzu sabırsızlıkla bekliyoruz ve umarız yakında Ekip Games™ ailesinin bir parçası olursunuz!</p>
                         <footer>Ekip Games™</footer>
                     `;
                 }
             }
 
-            // Opsiyonel: href yoksa tıklamayı engelle
+            // href yoksa tıklamayı engelle
             downloadLink.addEventListener('click', (event) => {
                 if (!downloadLink.getAttribute('href')) {
                     event.preventDefault();
